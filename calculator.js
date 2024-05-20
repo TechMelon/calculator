f7d2a0e88674565ff12579f0c7257281eab37345
sneha <snehabhardwaj035@gmail.com>
2024-05-20 20:15:00 +0530

    first commit

---
+++ calculator.js
@@ -0,0 +1,10 @@
+let res = 0;
+// document.getElementsByClassName("screen").textContent = res
+// const screenElement = document.querySelector('.screen');
+// screenElement.textContent = res;
+document.querySelector(".screen").textContent = res;
+
+function set(Number) {
+        const screenElement = document.querySelector('.screen');
+        screenElement.textContent = Number;
+    }
