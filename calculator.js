f66b739250f0216b0b1a8cffd6b4c420c74213f3
sneha <snehabhardwaj035@gmail.com>
2024-05-24 00:03:24 +0530

    new changes

--- calculator.js
+++ calculator.js
@@ -1,10 +1,22 @@
 let res = 0;
-// document.getElementsByClassName("screen").textContent = res
-// const screenElement = document.querySelector('.screen');
-// screenElement.textContent = res;
-document.querySelector(".screen").textContent = res;
+const screenElement = document.querySelector('.screen');
+screenElement.textContent = res;
 
 function set(Number) {
-        const screenElement = document.querySelector('.screen');
-        screenElement.textContent = Number;
+        screenElement.textContent += Number;
+        
     }
+    console.log(screenElement.textContent == 0);
+    // replace 0 onclicking first number on screen
+    // comparision between == and === 
+    // calculation should be perform on screen
+    // delete button should work
+
+
+
+    // problem statement summary
+    // detailed description of problem statement.., step wise with imgs if possible
+    // how to solve that problem and approch.., step wise
+    // what i tried and how i tried..,ss attaching compulsory step wise
+    // how much it is solved and where are my doubts(Doubts should be point wise).
+    
